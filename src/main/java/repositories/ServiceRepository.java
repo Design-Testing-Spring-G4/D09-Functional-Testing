75ae168a6345b6b751018af6568b10a4b2c3d913
Sermor95 <sermor95@gmail.com>
2018-03-22 12:27:35 +0100

    V1.3
    Update services about repositories

--- src/main/java/repositories/ServiceRepository.java
+++ src/main/java/repositories/ServiceRepository.java
@@ -2,7 +2,9 @@
 package repositories;
 
 import java.util.Collection;
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,7 +14,14 @@ import domain.Service;
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
 
+	//The best-selling services.
 	@Query("select s from Service s where s.requests.size=(select max(s.requests.size) from Service s)")
 	Collection<Service> bestSellingServices();
 
+	@Query("select (select count(s) from Service s where s.category != null)*1.0/count(t) from Trip t")
+	Double ratioTripsCancelled();
+
+	//The top-selling services.
+	@Query("select s from Service s order by s.requests.size desc")
+	List<Service> topSellingServices(Pageable pageable);
 }
